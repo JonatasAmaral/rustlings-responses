1cfe83c02723015d88a61e940b396701c20709d7
Jonatas Amaral <jonataspadtab@gmail.com>
2023-01-01 13:21:05 -0300

    exec quiz 1 (ifs):  17/94

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,15 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(quant:usize )->usize {
+    if quant <= 40 {
+        return quant * 2;
+    }
+
+    return quant;
+}
 
 // Don't modify this function!
 #[test]
