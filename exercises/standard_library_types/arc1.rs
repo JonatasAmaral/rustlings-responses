16b495752bff6dfc6466b7ab10194bf8452a69ea
Jonatas Amaral <jonataspadtab@gmail.com>
2023-01-01 13:21:05 -0300

    arc: 77/94

--- exercises/standard_library_types/arc1.rs
+++ exercises/standard_library_types/arc1.rs
@@ -18,23 +18,23 @@
 // where the second TODO comment is. Try not to create any copies of the `numbers` Vec!
 // Execute `rustlings hint arc1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 #![forbid(unused_imports)] // Do not change this, (or the next) line.
 use std::sync::Arc;
 use std::thread;
 
 fn main() {
     let numbers: Vec<_> = (0..100u32).collect();
-    let shared_numbers = // TODO
+    let shared_numbers = Arc::new(numbers);
     let mut joinhandles = Vec::new();
 
+    let the_closure =  |ch_nums: Arc<Vec<u32>>, offset: u32| {
+        let sum : u32 = ch_nums.iter().filter(|n| *n % 8 == offset).sum();
+        println!("Sum of offset {} is {}", offset, sum);
+    };
+
     for offset in 0..8 {
-        let child_numbers = // TODO
-        joinhandles.push(thread::spawn(move || {
-            let sum: u32 = child_numbers.iter().filter(|n| *n % 8 == offset).sum();
-            println!("Sum of offset {} is {}", offset, sum);
-        }));
+        let child_numbers = shared_numbers.clone();
+        joinhandles.push(thread::spawn(move || the_closure(child_numbers, offset)));
     }
     for handle in joinhandles.into_iter() {
         handle.join().unwrap();
