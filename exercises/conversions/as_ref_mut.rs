3e8bbe3e110dd85de77e7e827c3f52cc93313fc9
Jonatas Amaral <jonataspadtab@gmail.com>
2023-01-01 13:21:05 -0300

    as_ref_mut: 94/94

--- exercises/conversions/as_ref_mut.rs
+++ exercises/conversions/as_ref_mut.rs
@@ -3,8 +3,6 @@
 // and https://doc.rust-lang.org/std/convert/trait.AsMut.html, respectively.
 // Execute `rustlings hint as_ref_mut` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::ops::{Mul, MulAssign };
 
 // Obtain the number of bytes (not characters) in the given argument
